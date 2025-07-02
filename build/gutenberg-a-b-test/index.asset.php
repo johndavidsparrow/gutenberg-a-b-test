df5a303ecf14365233259d40ebca0dd413796565
John Sparrow <john.sparrow@gmail.com>
2025-07-02 16:28:29 -0700

    Initial commit

---
+++ build/gutenberg-a-b-test/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'f019937c466a38461f12');
---
+++ build/gutenberg-a-b-test/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'f5af9e0d3dec300fdf80');
