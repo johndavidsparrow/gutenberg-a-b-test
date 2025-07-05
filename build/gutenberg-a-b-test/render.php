2456a2395f3c3a22679745efd7b54ae5cd109130
John Sparrow <john.sparrow@gmail.com>
2025-07-04 23:44:06 -0700

    Apply weighting

--- build/gutenberg-a-b-test/render.php
+++ build/gutenberg-a-b-test/render.php
@@ -30,20 +30,34 @@ if ( $chosen_index === null ) {
 
     // Create an array of weightings
     $weightings = array();
+    $weightings_sum = 0;
     foreach( $parsed_blocks as $index => $child ) {
         $weightings[$index] = $child['attrs']['weighting'];
+        $weight_total += $child['attrs']['weighting'];
     }
-
-    print_r( $weightings );
-
-
+    if ( count( $weightings ) !== 2 || $weight_total !== 100 ) {
+        $weightings = [
+            0 => 50,
+            1 => 50
+        ];
+    }
 
     // Select a random weighted child and write cookie
     $rnd = rand(1, 100);
     $weight_total = 0;
+    foreach( $weightings as $index => $weight ) {
+        // Add to weight total
+        $weight_total += $weight;
+        if ( $rnd <= $weight_total ) {
+            $chosen_index = $index;
+            break;
+        }
+    }
 
+    echo 'Random number is ' . $rnd;
+    echo 'Chosen index is ' . $chosen_index;
 
-
+    // set cookie here TODO
 
 
 
