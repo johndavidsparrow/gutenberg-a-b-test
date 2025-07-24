1bc6efb0429c1bba17a519f4ebd384786768537a
John Sparrow <john.sparrow@gmail.com>
2025-07-24 16:34:52 -0700

    Production build

--- build/gutenberg-a-b-test/index.asset.php
+++ build/gutenberg-a-b-test/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a5a7b106744353e33396');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0e0f21f1a2e202caa792');
